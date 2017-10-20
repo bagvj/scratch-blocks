5aed4fb8e651fd619bd9f263d63c494249c07d93
zhouguang <zhouguang@kenrobot.com>
2017-10-20 14:11:31 +0800

    comment unused files

--- webpack.config.js
+++ webpack.config.js
@@ -17,52 +17,54 @@ module.exports = [{
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   }
-}, {
-  entry: {
-    horizontal: './shim/horizontal.js',
-    vertical: './shim/vertical.js'
-  },
-  output: {
-    library: 'Blockly',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist', 'web'),
-    filename: '[name].js'
-  }
 },
-{
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'gh-pages')
-  },
-  plugins: [
-      new CopyWebpackPlugin([{
-        from: 'node_modules/google-closure-library',
-        to: 'closure-library'
-      }, {
-        from: 'blocks_common',
-        to: 'playgrounds/blocks_common',
-      }, {
-        from: 'blocks_horizontal',
-        to: 'playgrounds/blocks_horizontal',
-      }, {
-        from: 'blocks_vertical',
-        to: 'playgrounds/blocks_vertical',
-      }, {
-        from: 'core',
-        to: 'playgrounds/core'
-      }, {
-        from: 'media',
-        to: 'playgrounds/media'
-      }, {
-        from: 'msg',
-        to: 'playgrounds/msg'
-      }, {
-        from: 'tests',
-        to: 'playgrounds/tests'
-      }, {
-        from: '*.js',
-        ignore: 'webpack.config.js',
-        to: 'playgrounds'
-      }])
-  ]
-}];
+// {
+//   entry: {
+//     horizontal: './shim/horizontal.js',
+//     vertical: './shim/vertical.js'
+//   },
+//   output: {
+//     library: 'Blockly',
+//     libraryTarget: 'umd',
+//     path: path.resolve(__dirname, 'dist', 'web'),
+//     filename: '[name].js'
+//   }
+// },
+// {
+//   output: {
+//     filename: '[name].js',
+//     path: path.resolve(__dirname, 'gh-pages')
+//   },
+//   plugins: [
+//       new CopyWebpackPlugin([{
+//         from: 'node_modules/google-closure-library',
+//         to: 'closure-library'
+//       }, {
+//         from: 'blocks_common',
+//         to: 'playgrounds/blocks_common',
+//       }, {
+//         from: 'blocks_horizontal',
+//         to: 'playgrounds/blocks_horizontal',
+//       }, {
+//         from: 'blocks_vertical',
+//         to: 'playgrounds/blocks_vertical',
+//       }, {
+//         from: 'core',
+//         to: 'playgrounds/core'
+//       }, {
+//         from: 'media',
+//         to: 'playgrounds/media'
+//       }, {
+//         from: 'msg',
+//         to: 'playgrounds/msg'
+//       }, {
+//         from: 'tests',
+//         to: 'playgrounds/tests'
+//       }, {
+//         from: '*.js',
+//         ignore: 'webpack.config.js',
+//         to: 'playgrounds'
+//       }])
+//   ]
+// }
+];
