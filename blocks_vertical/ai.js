0e42c50e605f4f0852095414fe0d7224b1c9e50f
zhouguang <zhouguang@kenrobot.com>
2018-05-22 14:50:59 +0800

    ai

--- blocks_vertical/ai.js
+++ blocks_vertical/ai.js
@@ -14,47 +14,11 @@ Blockly.Blocks['ai_playAudio'] = {
 			"args0": [
 				{
 					"type": "input_value",
-					"name": "AIAUDIO"
+					"name": "TEXT"
 				}
 			],
 			"category": Blockly.Categories.ai,
 			"extensions": ["colours_ai", "shape_statement"]
-		});
-	}
-};
-
-Blockly.Blocks['ai_picRecognition'] = {
-    init: function() {
-		this.jsonInit({
-			"message0": "2秒后识别 %1 图片",
-			"args0": [
-				{
-					"type": "field_dropdown",
-					"name": "AIPIC",
-					"options": [
-						['菜品', 'menuRecognition'],
-						['人脸', 'faceRecognition'],
-						['主体', 'mainRecognition'],
-						['植物', 'plantRecognition'],
-						['动物', 'animalRecognition'],
-						['文字', 'wordRecognition'],
-						['logo', 'logoRecognition']
-					]
-				}
-			],
-			"category": Blockly.Categories.ai,
-			"extensions": ["colours_ai", "shape_statement"]
-		});
-    }
-};
-
-Blockly.Blocks['ai_picResult'] = {
-	init: function() {
-		this.jsonInit({
-			"message0": "图片识别结果",
-			"category": Blockly.Categories.ai,
-			"checkboxInFlyout": true,
-			"extensions": ["colours_ai", "output_number"]
 		});
 	}
 };
@@ -67,11 +31,11 @@ Blockly.Blocks['ai_speechRecognition'] = {
 			"args0": [
 				{
 					"type": "field_dropdown",
-					"name": "AISPE",
+					"name": "SPEECH",
 					"options": [
-						['2秒', '3000'],
-						['3秒', '4000'],
-						['5秒', '6000']
+						['2秒', '2000'],
+						['3秒', '3000'],
+						['5秒', '5000']
 					]
 				}
 			],
@@ -87,6 +51,44 @@ Blockly.Blocks['ai_speechResult'] = {
 	init: function() {
 		this.jsonInit({
 			"message0": "语音识别结果",
+			"category": Blockly.Categories.ai,
+			"checkboxInFlyout": true,
+			"extensions": ["colours_ai", "output_number"]
+		});
+	}
+};
+
+Blockly.Blocks['ai_picRecognition'] = {
+    init: function() {
+		this.jsonInit({
+			"message0": "2秒后识别 %1 图片",
+			"args0": [
+				{
+					"type": "field_dropdown",
+					"name": "TAG",
+					"options": [
+						['菜品', 'menuRecognition'],
+						['人脸', 'faceRecognition'],
+						['主体', 'mainRecognition'],
+						['植物', 'plantRecognition'],
+						['动物', 'animalRecognition'],
+						['文字', 'wordRecognition'],
+						['logo', 'logoRecognition']
+					]
+				}
+			],
+			"category": Blockly.Categories.ai,
+			"colourSecondary": Blockly.Colours.ai.secondary,
+			"colourTertiary": Blockly.Colours.ai.tertiary,
+			"extensions": ["colours_ai", "shape_statement"]
+		});
+    }
+};
+
+Blockly.Blocks['ai_picResult'] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "图片识别结果",
 			"category": Blockly.Categories.ai,
 			"checkboxInFlyout": true,
 			"extensions": ["colours_ai", "output_number"]
